ec07402779fae077d1e822bf6a81c8e4a3b047e7
Samueldass <25005518@siswa.um.edu.my>
2026-05-22 13:45:46 +0800

    Add files via upload

---
+++ LibraryADT.java
@@ -0,0 +1,11 @@
+package smartlibrary;
+
+public interface LibraryADT {
+    void addBook(int isbn, String title, String author);
+    void searchBook(int isbn);
+    void borrowBook(int isbn);
+    void viewLatestHistory();
+    void displayAllBooks();
+    void clearHistory();
+    void clearAllBooks();
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package smartlibrary;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create the system and run the menu
+        SmartLibrary library = new SmartLibrary();
+        library.runMenu();
+    }
+}
